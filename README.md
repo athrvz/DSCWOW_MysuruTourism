3d2cbbb1edee379b3da1bccddc0b2bcf12e6c21b
athrvz <atharvazagade069@gmail.com>
2020-12-11 04:54:09 -0800

    fixing the changes

--- README.md
+++
@@ -1,2 +0,0 @@
-Mysuru Tourism Website
-This is a only a front-end version of the website.
---
+++ main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+// 	$('.menu-toggler').on('click', function(){
+// 		$(this).toggleClass('open');
+// 		$('.top-nav').toggleClass('open');
+// 	});
+// });
+
+$(document).ready(function(){
+	$('.menu-toggler').on('click', function(){
+		$(this).toggleClass('open');
+		$('.top-nav').toggleClass('open');
+		$('.nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+	});
+});
